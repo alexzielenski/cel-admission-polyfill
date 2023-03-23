e6e777fb8c6180c957465f9d76e1a5d530068299
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2023-03-22 23:01:06 -0700

    install new rds

--- global_debug.go
+++ global_debug.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/klog/v2"
 )
 
 //go:embed crds/celadmissionpolyfill.k8s.io_validationrulesets.yaml
@@ -18,7 +19,14 @@ var validationRuleSetsCRD string
 //go:embed crds/celadmissionpolyfill.k8s.io_policytemplates.yaml
 var policyTempaltesCRD string
 
+//go:embed crds/admissionregistration.polyfill.sigs.k8s.io_validatingadmissionpolicies.yaml
+var policyCRD string
+
+//go:embed crds/admissionregistration.polyfill.sigs.k8s.io_validatingadmissionpolicybindings.yaml
+var policyBindingCRD string
+
 func DEBUG_InstallCRDs(ctx context.Context, client dynamic.Interface) {
+	klog.Info("installing CRDs")
 	_, err := client.Resource(schema.GroupVersionResource{
 		Group:    "apiextensions.k8s.io",
 		Version:  "v1",
@@ -46,6 +54,40 @@ func DEBUG_InstallCRDs(ctx context.Context, client dynamic.Interface) {
 			"policytemplates.celadmissionpolyfill.k8s.io",
 			types.ApplyPatchType,
 			[]byte(policyTempaltesCRD),
+			metav1.PatchOptions{FieldManager: "cel-polyfill-controller"},
+		)
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = client.Resource(schema.GroupVersionResource{
+		Group:    "apiextensions.k8s.io",
+		Version:  "v1",
+		Resource: "customresourcedefinitions",
+	}).
+		Patch(
+			ctx,
+			"validatingadmissionpolicies.admissionregistration.polyfill.sigs.k8s.io",
+			types.ApplyPatchType,
+			[]byte(policyCRD),
+			metav1.PatchOptions{FieldManager: "cel-polyfill-controller"},
+		)
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = client.Resource(schema.GroupVersionResource{
+		Group:    "apiextensions.k8s.io",
+		Version:  "v1",
+		Resource: "customresourcedefinitions",
+	}).
+		Patch(
+			ctx,
+			"validatingadmissionpolicybindings.admissionregistration.polyfill.sigs.k8s.io",
+			types.ApplyPatchType,
+			[]byte(policyBindingCRD),
 			metav1.PatchOptions{FieldManager: "cel-polyfill-controller"},
 		)
 
